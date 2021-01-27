3381ef77e831a1adec6334c350c858d78b675de1
aravind <aravind.naidu@anclacorp.com>
2021-01-27 09:43:35 +0530

    Initial Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWebApplication.Models;
+
+namespace TestWebApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            if (Session["UserName"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserModel _user)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var apiRes = _user.UserRegistration(_user);
+                if (apiRes.IsSuccessful)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ViewBag.error = "Register Failed";
+                }
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin uLogin)
+        {
+            UserModel uObj = new UserModel();
+            if (ModelState.IsValid)
+            {
+                var apiRes = uObj.LoginDetails(uLogin);
+                if (apiRes.IsSuccessful)
+                {
+                    var resJ = apiRes.ContentLength;
+                    if (resJ > 2)
+                    {
+                        Session["UserName"] = uLogin.UserName.ToString();
+                        //if (resJ.Success)
+                        //{
+
+                        //}
+                        return RedirectToAction("Index");
+                    }
+                    else {
+                        ViewBag.error = "Invalid User";
+                        return View();
+                    }
+                   
+                }
+                else
+                {
+                    ViewBag.error = "Login Failed";
+                }
+            }
+            return View();
+        }
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,58 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApplication.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string gender { get; set; }
+        [Required]
+        public string phoneNumber { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string city { get; set; }
+
+        public IRestResponse UserRegistration(UserModel content)
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["WEBAPIURL"]);
+            var request = new RestRequest("user/CreateUser", Method.POST);
+            request.AddJsonBody(new { UserName = content.UserName, email_id = content.Email, Password = content.Password, gender = content.gender, phone_number = content.phoneNumber, city = content.city });
+            IRestResponse res = client.Execute(request);
+            Console.WriteLine(res);
+            return res;
+        }
+        public IRestResponse LoginDetails(UserLogin uObj)
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["WEBAPIURL"]);
+            var request = new RestRequest("user/UserLogin", Method.POST);
+            request.AddJsonBody(new { UserName = uObj.UserName, Password = uObj.Password });
+            IRestResponse res = client.Execute(request);
+            Console.WriteLine(res);
+            return res;
+        }
+    }
+    public class UserLogin
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
